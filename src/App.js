105f6c97b216ff7b71eccbc70cf14821f35718a7
ifty03 <ashikulislamb84@gmail.com>
2022-03-26 07:18:09 +0600

    added acordian

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Question from './Components/Question/Question';
 import {Shop} from './Components/Shop/Shop';
 
 function App() {
   return (
     <div style={{margin:"0px 30px"}}>
      <Shop></Shop>
+     <Question></Question>
     </div>
   );
 }
